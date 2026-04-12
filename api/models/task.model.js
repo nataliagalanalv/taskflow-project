328eddc1b6f496f30145371b1dc8e9ab540b2009
Natalia Galan <nataliagalan@gamail.com>
2026-04-12 08:24:53 +0200

    12.04 Prueba 26 Vercel con API

--- api/models/task.model.js
+++ api/models/task.model.js
@@ -6,6 +6,16 @@ const taskSchema = new mongoose.Schema({
     type: { type: String },
     completed: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now }
+}, {
+    toJSON: {
+        virtuals: true,
+        transform: (doc, ret) => {
+            ret.id = ret._id.toString(); 
+            delete ret._id;             
+            delete ret.__v;             
+            return ret;
+        }
+    }
 });
 
 module.exports = mongoose.models.Task || mongoose.model('Task', taskSchema);
