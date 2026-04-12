e95c138183d0e92fd056c9ccf0d2570293941d35
Natalia Galan <nataliagalan@gamail.com>
2026-04-12 08:17:35 +0200

    12.04 Prueba 24 Vercel con API

--- api/routes/task.routes.js
+++ api/routes/task.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const taskController = require('./controllers/task.controller');
+const taskController = require('../controllers/task.controller');
 
 // Definir rutas y conectarlas al controlador
 router.get('/', taskController.getTasks);
 router.post('/', taskController.createTask);
 router.delete('/:id', taskController.deleteTask);
 router.put('/:id', taskController.updateTask);
+router.patch('/:id/completar', taskController.completeTask);
 
 module.exports = router;
